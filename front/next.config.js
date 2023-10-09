eef1df11ea5e00e83339c8c02006801898958cba
sss388 <132276339+sss388@users.noreply.github.com>
2023-10-09 23:49:40 +0900

    aaa

--- front/next.config.js
+++ front/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        generateBuildId: async() => '4n0tCv8jfjnYFKb0jLd2P',
         domains: [
             "lh3.googleusercontent.com",
             "res.cloudinary.com"
         ]
+    },
+    generateBuildId: async () => {
+        return '4n0tCv8jfjnYFKb0jLd2P'
     },
 }
 
