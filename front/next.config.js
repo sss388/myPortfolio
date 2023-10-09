5f560be8c5144560a2204428f87b8e7af086352d
sss388 <132276339+sss388@users.noreply.github.com>
2023-10-09 23:47:41 +0900

    dd

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        generateBuildId: async() => 'constant-build-id',
+        generateBuildId: async() => '4n0tCv8jfjnYFKb0jLd2P',
         domains: [
             "lh3.googleusercontent.com",
             "res.cloudinary.com"
